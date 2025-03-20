92e221579059286145a5ab17d9460905bed02c94
chiqueto <lfchiqueto@gmail.com>
2025-03-20 13:49:56 -0300

    feat: start remodeling all

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
+import { MenuIcon } from "lucide-react";
+
 const Navbar = () => {
     return (
-        <header className="flex justify-between px-2 md:px-12 items-center text-2xl h-14 w-[100%] bg-amber-400 text-white">
-            <p className="font-impact text-2xl">Pokemon API</p>
-            <nav>
+        <header className="flex relative justify-center px-2 md:px-12 items-center text-2xl h-16 w-[100%] bg-base-red text-white">
+            <MenuIcon size={32} className="absolute left-2" />
+            <p className="font-impact text-3xl flex-1 items-center text-center">Pokedex</p>
+            {/* <nav>
                 <ul className="flex gap-2 font-navigation text-lg">
                     <li className="hover:underline hover:scale-105 transform transition-all duration-200 font-bold"><a href="/pokedex">Pokedex</a></li>
                     <li className="hover:underline hover:scale-105 transform transition-all duration-200 font-bold"><a href="/meutime">Monte seu time</a></li>
                 </ul>
-            </nav>
+            </nav> */}
         </header>
     );
 }
--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import { Pokemon } from "../types/Pokemon"
 import Loading from "../components/Loading";
+import { FilterIcon, SearchIcon } from "lucide-react";
+import Button from "../components/Button";
 
 const Pokedex = () => {
     const [pokemon, setPokemon] = useState<Pokemon>()
@@ -43,45 +45,52 @@ const Pokedex = () => {
     }
 
     return (
-        <div className="flex flex-col w-[100%] justify-center items-center">
-
-
-            <div className="flex flex-col justify-center items-center mt-4 bg-red-500 px-4 py-2 text-white">
-                <div className="flex flex-col justify-center items-center">
-                    <label className="text-lg font-bold" htmlFor="pokename">Busque pelo pokémon</label>
-                    <div className="flex flex-row gap-2">
-                        <input
-                            type="text"
-                            value={pokeName}
-                            name="pokename"
-                            placeholder="Pikachu"
-                            onChange={handleNameChange}
-                            className="bg-amber-100 border text-black border-black rounded-xl px-2 mt-2 focus:outline-1 focus:outline-amber-300 shadow-none"
-                        />
-                        <button onClick={loadApi} className="bg-red-500 border border-white mt-2 cursor-pointer hover:bg-red-600 shadow  shadow-black text-white px-2 py-1 rounded-2xl">Buscar</button>
-                    </div>
-                </div>
-                {loading ? (
-                    <Loading loading={loading} />
-                ) : (
-                    pokemon ? (
-                        <div className="w-full relative flex flex-col justify-center items-center p-4 rounded-xl">
-                            <div className="absolute w-full h-full bg-white opacity-50 backdrop-blur-lg"></div>
-                            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                            <div>Nome: {pokemon.name}</div>
-                            <div>Número: {pokemon.id}</div>
-                            <div>Peso: {pokemon.weight / 10} Kg</div>
-                            <div>Altura: {pokemon.height / 10}m</div>
-                        </div>
-                    ) : (
-                        <div>
-                            <h2>{errorMsg}</h2>
-                        </div>
-                    )
-                )}
-            </div>
+        <div className="mt-4 mx-2 flex flex-row justify-between items-center gap-3">
+            <input type="text" className="max-w-64 w-full bg-gray-100 border-base-red border shadow-sm rounded-xl h-10 focus:outline-1 focus:outline-base-red px-2 text-lg shadow-black/25" />
+            <Button />
+            <Button />
 
         </div>
+
+        // <div className="flex flex-col w-[100%] justify-center items-center">
+
+
+        //     <div className="flex flex-col justify-center items-center mt-4 bg-red-500 px-4 py-2 text-white">
+        //         <div className="flex flex-col justify-center items-center">
+        //             <label className="text-lg font-bold" htmlFor="pokename">Busque pelo pokémon</label>
+        //             <div className="flex flex-row gap-2">
+        //                 <input
+        //                     type="text"
+        //                     value={pokeName}
+        //                     name="pokename"
+        //                     placeholder="Pikachu"
+        //                     onChange={handleNameChange}
+        //                     className="bg-amber-100 border text-black border-black rounded-xl px-2 mt-2 focus:outline-1 focus:outline-amber-300 shadow-none"
+        //                 />
+        //                 <button onClick={loadApi} className="bg-red-500 border border-white mt-2 cursor-pointer hover:bg-red-600 shadow  shadow-black text-white px-2 py-1 rounded-2xl">Buscar</button>
+        //             </div>
+        //         </div>
+        //         {loading ? (
+        //             <Loading loading={loading} />
+        //         ) : (
+        //             pokemon ? (
+        //                 <div className="w-full relative flex flex-col justify-center items-center p-4 rounded-xl">
+        //                     <div className="absolute w-full h-full bg-white opacity-50 backdrop-blur-lg"></div>
+        //                     <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        //                     <div>Nome: {pokemon.name}</div>
+        //                     <div>Número: {pokemon.id}</div>
+        //                     <div>Peso: {pokemon.weight / 10} Kg</div>
+        //                     <div>Altura: {pokemon.height / 10}m</div>
+        //                 </div>
+        //             ) : (
+        //                 <div>
+        //                     <h2>{errorMsg}</h2>
+        //                 </div>
+        //             )
+        //         )}
+        //     </div>
+
+        // </div>
 
 
     )
